c344f7e4f5d58fd55070c8d4a9b8d768d75dcd91
xulitong22 <annexlt@yahoo.com>
2024-08-30 12:41:36 +0200

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,8 +44,39 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
             model_report: dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, 
-                                               y_test=y_test, models=models)
+                                               y_test=y_test, models=models, param=params)
             
             '''
             get the best model score and best model name from model_report
